e6e575b2dd0351b4c8faee95f303d48c6e82e82b
DENISE <DENISE@DENISE-PC>
2019-11-12 10:24:55 -0400

    teste segundo commit

--- Teste/src/com/db1Start/Main.java
+++ Teste/src/com/db1Start/Main.java
@@ -3,7 +3,7 @@ package com.db1Start;
 public class Main {
 
 	public static void main(String[] args) {
-		Exercicio exercicio =new Exercicio();
+		Exercicio exercicio = new Exercicio();
 		
 		exercicio.Inteiro(10,7);
 		exercicio.Texto("Db1 StarT");
